5db19e21b18d79bb58c5ef2ce585f7feae2f3edb
balanjesu <balan_jesu@yahoo.com>
2019-04-02 11:51:52 +0200

    test2

--- jesu/src/jesu/jesu.java
+++ jesu/src/jesu/jesu.java
@@ -1,8 +1,13 @@
 package jesu;
 
+
 public class jesu {
 int a=19;
 int b=21;
 int c= a+b;
+
+int d=a+b+c;
+
+
 
 }
---
+++ jesu/src/jesu/test.java
@@ -0,0 +1,8 @@
+package jesu;
+
+public class test {
+int a=10;
+int b=10;
+int c=a+b;
+
+}
